785826d1bab2b2c6e77acd5a6ecc7f2bd0c227c6
theSetox <stephen.siapno7@gmail.com>
2024-04-04 23:56:26 +0800

    Add delete button
    Add delete button UI.
    
    Test: Execute `koverHtmlReportDebug` and check if all of the method in PaymentDataRepository is covered.
    Test: Unit test is passing
    Test: Can assemble the project
    Test: No problem when running spotlessCheck
    Test: Can install and run the application

--- app/src/main/java/com/example/tipjar/view/component/history/ViewPaymentHistoryDialog.kt
+++ app/src/main/java/com/example/tipjar/view/component/history/ViewPaymentHistoryDialog.kt
@@ -2,22 +2,35 @@ package com.example.tipjar.view.component.history
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.example.tipjar.R
 import com.example.tipjar.model.entity.PaymentHistory
+import com.example.tipjar.view.buttonTextStyle
 import com.example.tipjar.view.screen.HistorySummary
+
+@Preview(showBackground = true)
+@Composable
+fun ViewPaymentHistoryDialogPreview() {
+    ViewPaymentHistoryDialog(PaymentHistory(timestamp = "", amount = 0F, tip = 0F, image = ""), {})
+}
 
 @Composable
 fun ViewPaymentHistoryDialog(
@@ -43,6 +56,29 @@ fun ViewPaymentHistoryDialog(
             Box(modifier.background(Color.White)) {
                 HistorySummary(paymentHistory)
             }
+            Spacer(Modifier.size(12.dp))
+            DeletePaymentButton {}
         }
+    }
+}
+
+@Composable
+private fun DeletePaymentButton(onDelete: () -> Unit) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier =
+            Modifier
+                .fillMaxWidth()
+                .background(
+                    color = Color.Red,
+                    shape = RoundedCornerShape(16.dp),
+                )
+                .clickable { onDelete() },
+    ) {
+        Text(
+            text = "Delete Payment",
+            style = buttonTextStyle,
+            modifier = Modifier.padding(vertical = 16.dp),
+        )
     }
 }
